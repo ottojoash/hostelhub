487949424f2aa200a7ef711541d1030f9db3d7c4
ottojoash <louisjoshbricks@gmail.com>
2023-02-14 14:46:18 +0300

    register

--- src/Register.js
+++ src/Register.js
@@ -3,7 +3,14 @@ import React, {useState} from 'react'
 import { firebase} from '../config'
 
 const Register = () => {
-    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [firstname, setFirstName] = useState('')
+    const [lastname, setLastName] = useState('')
+
+    registerUser = async (email, password, firstname, lastname)=> {
+        
+    }
 }
 
 export default Register
