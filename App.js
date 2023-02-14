54cc8ca754acc8317063f7ea1b4fa97f94052193
ottojoash <louisjoshbricks@gmail.com>
2023-02-14 13:10:25 +0300

    login

--- App.js
+++ App.js
@@ -4,4 +4,8 @@ import React,{useState, useEffect} from "react";
 import {firebase} from './config';
 
 import Login from './src/Login';
-import Register from './src/Register'
+import Register from './src/Register';
+import Dashboard from './src/Dashboard';
+import Header from "./components/Header";
+
+const Stack
