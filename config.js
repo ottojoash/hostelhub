4e0e768989dbe34a31fff7153adcf737eff5baaf
ottojoash <louisjoshbricks@gmail.com>
2023-02-14 12:36:42 +0300

    firebase

--- config.js
+++ config.js
@@ -6,3 +6,23 @@ import 'firebase/compat/firestore';
 
 
 //for web
+const firebaseConfig = {
+    apiKey: "AIzaSyB53LjeC3iavNfKlMtZ1qYFI6PqcKoraDA",
+
+  authDomain: "hostelhub-fdaca.firebaseapp.com",
+
+  projectId: "hostelhub-fdaca",
+
+  storageBucket: "hostelhub-fdaca.appspot.com",
+
+  messagingSenderId: "451830684626",
+
+  appId: "1:451830684626:web:b9afb50f61626304db460b",
+
+  measurementId: "G-LZL05J6B3Q"
+
+}
+
+if (!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig)
+}
