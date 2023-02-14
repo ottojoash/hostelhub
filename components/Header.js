0c3445b6facc4f8897efadb22f270eb3f16ab558
ottojoash <louisjoshbricks@gmail.com>
2023-02-14 13:01:52 +0300

    header

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import { view, Text} from 'react-native'
+import React from 'react'
+
+const Header =() =>{
+    return(
+        <view>
+            <Text>Header</Text>
+        </view>
+    )
+}
+
+export default Header
