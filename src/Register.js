49fae0bb2914ec4de83dfb65620b082463bc0d5b
ottojoash <louisjoshbricks@gmail.com>
2023-02-14 15:14:17 +0300

    register

--- src/Register.js
+++ src/Register.js
@@ -79,3 +79,29 @@ const Register = () => {
 }
 
 export default Register
+
+const styles = StyleSheet.create({
+    container:{
+        flex :1,
+        alignItems: 'center',
+        marginTop:100,
+    },
+    textInput:{
+        paddingTop:20,
+        paddingBottom:10,
+        width:400,
+        fontSize:20,
+        borderBottomColor: '#000',
+        marginBottom:10,
+        textAlign:'center'
+    },
+    button:{
+        marginTop:50,
+        height:70,
+        width:250,
+        backgroundColor:'#026efd',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:50,
+    }
+})
