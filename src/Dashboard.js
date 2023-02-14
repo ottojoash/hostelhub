7c0e14459e5527ff892f773fa895ad300ffa24c8
ottojoash <louisjoshbricks@gmail.com>
2023-02-14 15:28:25 +0300

    dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,13 +1,39 @@
 import {View, Text,StyleSheet,SafeAreaView} from 'react-native'
 import React, {useState,useEffect} from 'react'
 import {firebase} from '../config'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const Dashboard = () => {
+    const [name, setName] = useState('')
+    
+    useEffect(()=>{
+        firebase.firestore().collection('users')
+        .doc(firebase.auth().currentUser.uid).get()
+        .then((snapshot)=>{
+            if(snapshot.exists){
+                setName(snapshot.data())
+            }
+            else{
+                console.log('user doesnot exist')
+            }
+            
+        })
+    },[])
+
     return(
-        <View>
-            <Text>Dashboard</Text>
-        </View>
-    )    
+        <SafeAreaView style={StyleSheet.container}>
+            <Text style={{fontSize:20, fontWeight:'bold'}}>
+                Hello,{name.firstname}
+            </Text>
+            <TouchableOpacity
+                onPress={()=> firebase.auth().signOut()}
+            >
+                <Text style={{fontSize:20,fontWeight:'bold'}}>
+                    Sign Out
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
 }
 
 export default Dashboard
