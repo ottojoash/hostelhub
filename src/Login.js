3e20811aa1f63041af718327148d5126acae204e
ottojoash <louisjoshbricks@gmail.com>
2023-02-14 14:33:03 +0300

    style

--- src/Login.js
+++ src/Login.js
@@ -61,4 +61,25 @@ const Login = () => {
 export default Login
 
 
-const styles = 
+const styles = StyleSheet.create({
+    container:{
+        flex :1,
+        alignItems: 'center',
+        marginTop:100,
+    }
+    textInput:{
+        paddingTop:20,
+        paddingBottom:10,
+        width:400,
+        fontSize:20,
+        borderBottomColor: '#000',
+        marginBottom:10,
+        textAlign:'center'
+    },
+    button:{
+        marginTop:50,
+        height:70,
+        width:250,
+        backgroundColor:'#'
+    }
+})
